7db7fb1916da15a2cf1adec86219fd12c810f6ac
simran17-0 <simransharma2105@gmail.com>
2026-01-10 18:42:41 +0530

    Added simple click interaction for apply button

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-  let applyButtons = document.querySelectorAll(".apply-btn");
-  let registerButtons = document.querySelectorAll(".register-btn");
-
-  applyButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      alert("Application submitted successfully!");
+    document.getElementById("applyBtn").addEventListener("click", function () {
+        alert("Application submitted successfully!");
     });
-  });
-
-  registerButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      alert("Registered successfully!");
-    });
-  });
-
 });
